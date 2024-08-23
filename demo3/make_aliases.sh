dd9c12f373dd92a52d06e7c9bd4115c6be78db35
Alex <alxzent@gmail.com>
2024-08-23 11:41:54 -0700

    Erase

--- demo3/make_aliases.sh
+++
@@ -1,6 +0,0 @@
-alias sw="docker exec -it clab-mod1-devconfig-switch"
-alias h1="docker exec -it clab-mod1-devconfig-host1"
-alias h2="docker exec -it clab-mod1-devconfig-host2"
-
-
-
--- lab1/part1/provided/make_dirs.sh
+++
@@ -1,5 +0,0 @@
-mkdir lab-host1
-mkdir lab-host2
-mkdir lab-host3
-mkdir lab-host4
-mkdir lab-switch
